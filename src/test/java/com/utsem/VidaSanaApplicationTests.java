727e3d4751433552a17a2d4fcf717f3c9c22ff01
Amairani <amairani00diego@gmail.com>
2025-02-07 20:35:23 -0600

    Subir el proyecto a github

---
+++ src/test/java/com/utsem/VidaSanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utsem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaSanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
